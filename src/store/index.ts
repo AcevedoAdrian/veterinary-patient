264bcc5304430a5d4b19f543c5f4ae9b7d122627
AcevedoAdrian <acevedohugoadrian@gmail.com>
2024-09-06 12:47:00 -0300

    feat: Add Veterinary Patient Application

---
+++ src/store/index.ts
@@ -0,0 +1,7 @@
+import {create } from 'zustand';
+
+export const usePatientStore = create( () => ({
+  // State: variables that will be used in the app
+
+  // Actions: functions that modify the state
+}))
